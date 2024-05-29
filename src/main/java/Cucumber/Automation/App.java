f65c1eb7b5e456e96c502afcec1210e24531a0e7
Akash <akashpatil1112@gmail.com>
2024-05-29 15:36:02 +0530

    changes made first time

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -1,13 +1,16 @@
 package Cucumber.Automation;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        System.out.println( "Adding another test" );
     }
+    
+    
+    
+    
 }

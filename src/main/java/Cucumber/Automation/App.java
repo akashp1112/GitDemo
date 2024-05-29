a349719bb16c7c70dab494b64ab7991c21e36e88
Akash <akashpatil1112@gmail.com>
2024-05-29 16:55:51 +0530

    Constructor added

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -10,8 +10,15 @@ public class App
         System.out.println( "Adding another test" );
         
         System.out.println("Adding second test in git");
+        
+        App a=new App();
+        
+        
     }
     
+    public App() {
+    	System.out.println("constructor made in class");
+    }
     
     
     
